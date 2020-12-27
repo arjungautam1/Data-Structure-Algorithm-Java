fd49f2802a60b450b15cecb5f5960e3eb9fcf81b
arjun-sudo <arjungautam5431@gmail.com>
2020-12-27 20:00:16 +0545

    Leetcode reverse number

--- src/java_interview/reverse_integer/Reverse_Integer_LeetCode.java
+++ src/java_interview/reverse_integer/Reverse_Integer_LeetCode.java
@@ -5,8 +5,23 @@
  * Time : 12:54
  */
 package java_interview.reverse_integer;
+/*Leet code approach to reverse the integer*/
+class Reverse_Integer_LeetCode {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+        return rev;
+    }
+    public static void main(String[] args) {
+        Reverse_Integer_LeetCode obj = new Reverse_Integer_LeetCode();
 
-public class Reverse_integer_leetcode {
-    
+        int result = obj.reverse(-12);
+        System.out.println(result);
+
+    }
+
 }
-
