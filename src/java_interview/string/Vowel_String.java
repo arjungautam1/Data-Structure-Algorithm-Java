f301452d28fc473e7af8deea029e9d6c0657b131
arjun-sudo <arjungautam5431@gmail.com>
2021-01-01 07:29:42 +0545

    End of 2020

--- src/java_interview/string/Vowel_String.java
+++ src/java_interview/string/Vowel_String.java
@@ -1,48 +1,12 @@
-package java_interview.string;// Java program to find all substring that
-// contain all vowels
-import java.util.HashSet;
+/**
+ * @author :arjun
+ * Project :Data-Structure-Algorithm-Java
+ * Date : 2020-12-31
+ * Time : 20:59
+ */
 
+package java_interview.string;
+//Program to find the vowel substring in java
 public class Vowel_String {
 
-    // Returns true if x is vowel. 
-    static boolean isVowel(char x) {
-        // Function to check whether a character is
-        // vowel or not
-        return (x == 'a' || x == 'e' || x == 'i'
-                || x == 'o' || x == 'u');
-    }
-
-    static void findSubstring(String str) {
-        HashSet<Character> hash = new HashSet<Character>();
-        // To store vowels
-
-        // Outer loop picks starting character and
-        // inner loop picks ending character.
-        int n = str.length();
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-
-                // If current character is not vowel,
-                // then no more result substrings
-                // possible starting from str[i].
-                if (isVowel(str.charAt(j)) == false)
-                    break;
-
-                // If vowel, then we insert it in hash
-                hash.add(str.charAt(j));
-
-                // If all vowels are present in current
-                // substring
-                if (hash.size() == 5)
-                    System.out.print(str.substring(i, j + 1) + " ");
-            }
-            hash.clear();
-        }
-    }
-
-    // Driver code
-    public static void main(String[] args) {
-        String str = "aeoibsddaeiouudb";
-        findSubstring(str);
-    }
 }
