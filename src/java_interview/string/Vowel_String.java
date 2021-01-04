66210596ad08eee06ba482ba6694bd6db17c8bb0
arjun-sudo <arjungautam5431@gmail.com>
2021-01-04 08:12:53 +0545

    Vowel substring

--- src/java_interview/string/Vowel_String.java
+++ src/java_interview/string/Vowel_String.java
@@ -6,7 +6,36 @@
  */
 
 package java_interview.string;
+
+import java.util.HashSet;
+
 //Program to find the vowel substring in java
 public class Vowel_String {
+
+    static boolean isVowel(char x) {
+        return (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u');
+    }
+    static void findSubString(String str){
+        HashSet<Character> hashSet=new HashSet<Character>();
+
+        int start=0;
+        for(int i=0;i<str.length();i++){
+            if(isVowel(str.charAt(i))==true){
+                hashSet.add(str.charAt(i));
+
+                if(hashSet.size()==5)
+                    System.out.println(str.substring(start,i+1)+" ");
+            }
+            else {
+                start=i+1;
+                hashSet.clear();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String str="aeoibsddaeiouudb";
+        findSubString(str);
+    }
 
 }
