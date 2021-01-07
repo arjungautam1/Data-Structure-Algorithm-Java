c95892aa041e39aaf2b4b306026e79318f7f6da9
arjun-sudo <arjungautam5431@gmail.com>
2021-01-07 10:21:44 +0545

    Palindrome revised

--- src/java_interview/palindrome/Palindrome.java
+++ src/java_interview/palindrome/Palindrome.java
@@ -2,19 +2,17 @@ package java_interview.palindrome;
 
 public class Palindrome {
     public static void main(String[] args) {
-        int n=456,sum=0;
-        int temp=n;
-        while(n>0){
-            int r=n%10;
-            sum=sum*10+r;
-            n=n/10;
-        }
-        if(temp==sum){
-            System.out.println("Palindrome");
-        }
-        else {
-            System.out.println("Not Palindrome");
-        }
+        int num = 10, rev = 0;
+        int originalInt = num;
 
+        while (num != 0) {
+            int remainder = num % 10;
+            rev = num * 10 + remainder;
+            num = num / 10;
+        }
+        if (originalInt == rev)
+            System.out.println("Number is Palindrome.");
+        else
+            System.out.println("Number is not Palindrome");
     }
 }
