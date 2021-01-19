8988b298a47d9316d04a3517b472cede7e514e88
arjun-sudo <arjungautam5431@gmail.com>
2021-01-19 08:21:12 +0545

    String to date

--- src/java_interview/string/StringToDate.java
+++ src/java_interview/string/StringToDate.java
@@ -4,5 +4,24 @@
  * Date : 2021-01-11
  * Time : 06:44
  */
-package java_interview.string;public class StringToDate {
+package java_interview.string;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StringToDate {
+
+    public static void main(String[] args) {
+        SimpleDateFormat formatter=new SimpleDateFormat("dd-MMM-yyyy");
+        String dateString="21-Jan-2021";
+        try {
+            Date date=formatter.parse(dateString);
+            System.out.println(date);
+            System.out.println(formatter.format(date));
+        }
+        catch (ParseException e){
+            e.printStackTrace();
+        }
+    }
 }
