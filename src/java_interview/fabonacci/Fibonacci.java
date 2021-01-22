f93b5130992d44a2d4f5ba6930fd8cd3109688ce
arjun-sudo <arjungautam5431@gmail.com>
2021-01-22 12:25:18 +0545

    formated

--- src/java_interview/fabonacci/Fibonacci.java
+++ src/java_interview/fabonacci/Fibonacci.java
@@ -11,16 +11,16 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         int n, first = 0, next = 1;
-        System.out.println("Enter how many fibo numbers to print:");
+        System.out.print("Enter how many fibo numbers to print:");
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
-        System.out.println("The first " + n + "Fibonacci numbers are:");
-        System.out.println(first + " " + next);
-        for (int i = 1; i < n - 2; ++i) {
+        System.out.print("The first " + n + " Fibonacci numbers are:");
+        System.out.print(first + " " + next);
+        for (int i = 1; i < n - 1; ++i) {
             int sum = first + next;
             first = next;
             next = sum;
-            System.out.println(" " + sum);
+            System.out.print(" " + sum);
         }
     }
 
